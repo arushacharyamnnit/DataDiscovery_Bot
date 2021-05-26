71d18f77daa46468d94f5b0b7a54753638dc5742
Arush Acharya <arushacharya459@gmail.com>
2021-05-26 18:56:48 +0530

    ui changed

--- Server/app.py
+++ Server/app.py
@@ -1,9 +1,26 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect,url_for,request
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home():
     return render_template('index.html')
+
+@app.route("/get")
+#function for the bot response
+def get_bot_response():
+    return f"hello"    
+
+# @app.route('/',methods=['POST','GET'])    
+# def login():
+#     if request.method=='POST':
+#         user=request.form['data']
+#         return redirect(url_for('user',usr=user))
+#     else:
+#         return render_template('index.html')    
+  
+# @app.route("/<usr>")
+# def user(usr):
+#     return f"<h1>{usr}</h1>"
 
 if __name__=="__main__":
     app.run(debug=True,port=8000)    
